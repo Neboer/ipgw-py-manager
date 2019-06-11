8621b984aa01dcaaa7bcc171ae462624ee287473
neboer <rubinposter@gmail.com>
2019-06-11 18:40:39 +0800

    修改了登录方法，增加了获取cookie的手段。准备使用session的cookieJar完成请求。使用python代替js来实现cookie的填充。正在进一步分析js代码。

--- sessionreq.py
+++ sessionreq.py
@@ -2,26 +2,45 @@ import requests
 from bs4 import BeautifulSoup, Tag
 
 
+def bake_cookie(cname, cvalue, until):
+    build_cookie = {cname: cvalue, "expires": until}
+    return build_cookie
+
+
 def pass_authenticate(username, password, session: requests.Session):
     r = session.get('http://ipgw.neu.edu.cn/srun_cas.php?ac_id=1')
     soup = BeautifulSoup(r.text, 'html.parser')
     form: Tag = soup.find("form", {'id': 'loginForm'})
     destination = form.attrs['action']
     ext_lt_string = form.find("input", {'id': 'lt'}).attrs["value"]
+    execution = form.find("input", {'name': 'execution'}).attrs["value"]
+    print(execution)
     formData = {
         'rsa': username + password + ext_lt_string,
         'ul': len(username),
         'pl': len(password),
         'lt': ext_lt_string,
-        'execution': 'e5s1',
+        'execution': execution,
         '_eventId': 'submit'
     }
-    print(destination)
+    return session.post("https://pass.neu.edu.cn" + destination, data=formData)
 
 
 connect_session = requests.Session()
+connect_session.headers.update({'User-Agent':
+                                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36',
+                                "Origin": "https://pass.neu.edu.cn",
+                                "Referer": "https://pass.neu.edu.cn/tpass/login?service=https%3A%2F%2Fipgw.neu.edu.cn%2Fsrun_cas.php%3Fac_id%3D3",
+                                "Upgrade-Insecure-Requests": "1",
+                                "Host": "pass.neu.edu.cn"
+                                })
 connect_session.get('http://ipgw.neu.edu.cn/srun_portal_pc.php?ac_id=1&')
-pass_authenticate('20183345', 'dddsddfs', connect_session)
+count = open("resources/count", "r").readlines()
+res = pass_authenticate(count[0], count[1], connect_session).text
+print(connect_session)
+with open("out.html", "w") as page:
+    page.write(res)
+# print(connect_session)
 
 # with open("out.html",'w') as file:
 #     file.write(r.text)
