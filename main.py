9782e63cf3813dbed876fc8dd7e81efcb7000b63
neboer <rubinposter@gmail.com>
2019-06-18 00:05:42 +0800

    修复了解析其他账号登录时因为解析顺序不同而导致的一个错误。

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse, getpass
 from sessionreq import connect_to_ipgw_by_unpw
 from parse_login_result import parse_login_result, get_devices_data, other_account, base_info, drop_device
 
-parser = argparse.ArgumentParser(description="NEU ipgw manage client")
+parser = argparse.ArgumentParser(description="NEU ipgw managing client")
 parser.add_argument('-v', '--version', action='version', version='NEU ipgw manage client V1.0 by Neboer')
 parser.add_argument('-i', action='store_true', help='login with stored NEU-pass username and password',
                     dest='login')
@@ -13,7 +13,7 @@ parser.add_argument('--self', action='store_true', help='if set, logout or query
 parser.add_argument('--other', action='store_true', help='if set, just logout or query your other devices.')
 parser.add_argument('--logout', default=None, help='logout specified id', dest='uid')
 parser.add_argument('-q', '--quiet', action='store_true', default=None, help='quiet mode, no output')
-parser.add_argument('-c', '--current', default=None, help='list devices and show the detail info of every device')
+parser.add_argument('-c', '--current', default=None, help='list devices and show the detail info of each device')
 parser.add_argument('-s', '--status', action='store_true', default=None,
                     help='show the detail of your ipgw account, needs web center\'s password. ')
 parser.add_argument('--config', action='store_true', help='open configure file with a text editor.')
@@ -29,9 +29,10 @@ def normal_login(username, password):
         ifOtherAccountLoginUid = other_account(result)
         if ifOtherAccountLoginUid:
             print("other account login, uid:" + ifOtherAccountLoginUid)
-        basicInformation = base_info(login_data_soup)
-        print("account: {}\nip: {}\nconsumed: {}\nonline: {}\n".format(basicInformation[0], basicInformation[1],
-                                                                       basicInformation[2], basicInformation[3]))
+        else:
+            basicInformation = base_info(login_data_soup)
+            print("account: {}\nip: {}\nconsumed: {}\nonline: {}\n".format(basicInformation[0], basicInformation[1],
+                                                                           basicInformation[2], basicInformation[3]))
         for device in get_devices_data(login_data_soup):
             print(device)
 
