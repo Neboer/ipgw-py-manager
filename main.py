58dca02825c539c7d8e7af9d4234bb872155064a
neboer <rubinposter@gmail.com>
2019-06-22 20:14:55 +0800

    整理了代码，增加了可读性。把函数分散开来。

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
 ﻿#!/usr/bin/env python3
-import argparse, getpass, os, requests, json
-from login import login, read_settings_into_session, print_fail_auth, print_login_successful, read_unpw_from_settings
-from requestresult import UnionAuth, SuccessPage, Device
+import os, requests, json
+from show_result import read_settings_into_session
+from login_process import whole_process
+from argument import parse_argument
+from requestresult import Device
 
-parser = argparse.ArgumentParser(description="NEU ipgw managing client")
-parser.add_argument('-v', '--version', action='version',
-                    version='NEU ipgw manage client V4.0 formal edition by Neboer!')
-parser.add_argument('-i', '--login', action='store_true', help='login with stored NEU-pass username and password',
-                    dest='login')
-parser.add_argument('-u', '--username', default=None, help='set a username to login/logout', dest='username')
-parser.add_argument('-o', action='store_true', help='log out all devices connected to the network.', dest='logout_all')
-parser.add_argument('--nocookie', action='store_true', help='request without cookies and neither store them')
-parser.add_argument('--recookie', action='store_true', help='request without cookies but store them')
-# parser.add_argument('--self', action='store_true', help='if set, logout or query your current device only.')
-# parser.add_argument('--other', action='store_true', help='if set, just logout or query your other devices.')
-parser.add_argument('--logout', default=None, help='logout specified id', dest='uid')
-parser.add_argument('--other', action='store_true', help='logout other account which logged on current ip.')
-# parser.add_argument('-c', '--current', action="store_true", help='list devices and show the detail info of each device')
-# parser.add_argument('-s', '--status', action='store_true', default=None,
-#                     help='show the detail of your ipgw account, needs web center\'s password. ')
-parser.add_argument('--config', default=None, help='open configure file with specific text editor.')
-args = parser.parse_args()
+args = parse_argument()
 
-setting_file_location = os.path.join( os.getenv("HOME") , ".ipgw-py-manager", "settings.json" )
+setting_file_location = os.path.join(os.getenv("HOME"), ".ipgw-py-manager", "settings.json")
 with open(setting_file_location, "r") as file:
     settings = json.load(file)
 flags_sum = args.login + args.logout_all + (args.uid is not None)
@@ -44,22 +29,7 @@ else:
     cookie_is_set = True
 
 if args.login:
-    if args.username:  # 如果用户指定了用户名，则用指定的用户名登录
-        password = getpass.getpass()
-        username = args.username
-    else:
-        username, password = read_unpw_from_settings(settings)
-    if (not username) or (not password):
-        print("username or password is empty, exit.")
-        exit(1)
-    result = login(global_session, username, password)
-    if type(result) is UnionAuth:
-        print_fail_auth(result)
-    elif type(result) is SuccessPage:
-        result.refresh(global_session)
-        print_login_successful(result)
-    else:
-        raise NameError("unknown response.")
+    whole_process(args.username, global_session, settings, True)
 
 if args.uid:
     if Device.logout_sid(args.uid, global_session):
@@ -68,25 +38,7 @@ if args.uid:
         print("logout {} error".format(args.uid))
 
 if args.logout_all:
-    if args.username:  # 如果用户指定了用户名，则用指定的用户名登录
-        password = getpass.getpass()
-        username = args.username
-    else:
-        username, password = read_unpw_from_settings(settings)
-    if (not username) or (not password):
-        print("username or password is empty, exit.")
-        exit(1)
-    result = login(global_session, username, password)
-    if type(result) is UnionAuth:
-        print_fail_auth(result)
-    elif type(result) is SuccessPage:
-        for device in result.device_list:
-            if device.logout(global_session):
-                print("logout {} successful".format(device.sid))
-            else:
-                print("logout {} error".format(device.sid))
-    else:
-        raise NameError("unknown response.")
+    whole_process(args.username, global_session, settings, False)
 
 if not args.nocookie:
     new_cookies = requests.utils.dict_from_cookiejar(global_session.cookies)
---
+++ show_result.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+from requestresult import SuccessPage, UnionAuth
+
+
+def distinguish_and_build(page_soup: BeautifulSoup):
+    title = page_soup.find("title").text
+    if title == "系统提示" or title == "智慧东大--统一身份认证":
+        return UnionAuth(page_soup)
+    elif title == "IP控制网关":
+        return SuccessPage(page_soup)
+    else:
+        return None
+
+
+def print_login_successful(page: SuccessPage):
+    if page.status == 0:
+        if page.online_other_uid:
+            print("other online, uid:", page.online_other_uid)
+        else:
+            print("account: {}\nip: {}\nconsumed: {}\nonline: {}\n".format(page.base_info[0], page.base_info[1],
+                                                                       page.base_info[2], page.base_info[3]))
+        for device in page.device_list:
+            print(device)
+    elif page.status == 1:
+        print("account locked, unlock first.")
+    elif page.status == 2:
+        print("insufficient funds.")
+
+
+def print_fail_auth(page: UnionAuth):
+    if page.locked:
+        print("fail 5 times, account locked for 1 min.")
+        return
+    print("username or password error, last try {} times.".format(page.last_temp))
+
+
+def read_settings_into_session(settings, session: requests.Session, set_cookie: bool,
+                               set_headers: bool = True):
+    cookie_dict = settings["ipgw_cookie_jar"]
+    header_dict = settings["global_headers"]
+    if set_cookie:
+        requests.utils.cookiejar_from_dict(cookie_dict, session.cookies)
+    session.headers.update(header_dict)
+
+
+def read_unpw_from_settings(settings: dict):
+    return settings["unity_login"]["username"], settings["unity_login"]["password"]
